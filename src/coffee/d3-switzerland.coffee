a0c70992840ed80a547d8537d93043788271db06
Dominique Rahm <dr@oxon.ch>
2015-05-16 22:30:27 +0200

    almost done

--- src/coffee/d3-switzerland.coffee
+++ src/coffee/d3-switzerland.coffee
@@ -1,167 +1,127 @@
-show = (elem) ->
-  region = elem.getAttribute('data-region')
-  svg = d3.select('svg')
-  d3.selectAll('g').style("display", "none")
-  svg.select('#' + region).style("display", "block")
+@years = [2010, 2011, 2012, 2013, 2014]
+@year = 2014
+@field = 'Informatik'
+@type = 'EFZ'
+@gender = 'both'
+@value = 'total'
 
-resize = ->
-  console.log 'resize '
-  # adjust things when the window size changes
-  svg = d3.select('svg')
-  width = parseInt(svg.style('width'))
+@regions = []
 
-  # scale each individual object according to theire own width/height and
-  # window width/height
-  # d3.selectAll("g").attr("transform", "scale(" + width / 600 + "), translate(" + width / 2 + ", 0)");
-  # probably all regions need ratio and translate
+@cantonsObjects = {'ZH' : {},'BE' : {},'LU' : {},'UR' : {},'SZ' : {},'OW' : {},'NW' : {},'GL' : {},
+'ZG' : {},'FR' : {},'SO' : {},'BS' : {},'BL' : {},'SH' : {},'AR' : {},'AI' : {},'SG' : {},'GR' : {},'AG' : {},
+'TG' : {},'TI' : {},'VD' : {},'VS' : {},'NE' : {},'GE' : {},'JU' : {}}
 
-  # translate(-centerX*(factor-1), -centerY*(factor-1))
-  # scale(factor)
-  ratio = 1
-  ratio = 1.25 if window.innerWidth <= 750
-  test = width * -1
-  # d3.selectAll("g").attr("transform", "scale(" + ratio + ")");
-  svg.select('#zentralschweiz').attr("transform", "scale(" + ratio + "), translate(" + @regions['Zentralschweiz'].origin + ")");
-  svg.select('#mittelland').attr("transform", "scale(" + ratio + "), translate(" + @regions['Mittelland'].origin + ")");
-  svg.select('#ostschweiz').attr("transform", "scale(" + ratio + "), translate(" + @regions['Ostschweiz'].origin + ")");
-  svg.select('#westschweiz').attr("transform", "scale(" + ratio + "), translate(" + @regions['Westschweiz'].origin + ")");
+@svg = d3.select('svg')
 
-
-  # map.style('width', width + 'px').style 'height', height + 'px'
-  # # resize the map
-  # map.select('.land').attr 'd', path
-  # map.selectAll('.state').attr 'd', path
-  return
+tip = d3.select("body").append("div")
+    .attr("class", "tip")
+    .style("opacity", 0);
 
 d3.select(window).on 'resize', resize
 
 # Scale
 windowWidth = window.innerWidth
 
-if windowWidth <= 750
-  console.log 'Mobile'
-  # d3.select('g').attr transform: 'translate(-120,0)'
-  # d3.selectAll
+# if windowWidth <= 750
+$(document).ready =>
+  dataCounter = 0
+  for index, year of @years
+    console.log year
+    # loadData year. then ->
+    #   dataCounter++
+    #   if dataCounter is @years.length
+    #     loadMap()
+  loadData(2014).then ->
+    loadSwitzerland().then ->
+      loadRegions().then ->
+        draw()
 
-@regions = []
-@regions[0] = '1'
+loadData = (year) ->
+  deffered = jQuery.Deferred()
+  d3.csv "#{year}.csv", (d) ->
 
-# Load world data
-d3.json 'ch.json', (err, geojson_switzerland) ->
-  if err
-    return console.error(err)
-  console.log 'Step 1 - loading GeoJSON data', geojson_switzerland
+    field = d.Ausbildungsfeld
+    type = d.Ausbildungstyp
 
-  # Load my data
-  d3.csv 'data.csv', ((d) ->
-    for k of d
-      if k != 'canton'
-        d[k] = +d[k]
-    d
-  ), (err, @data) =>
+    obj = @cantonsObjects[d.Canton];
+    if obj
+      if not obj[year]
+        obj[year] = {}
+      if not obj[year][field]
+        obj[year][field] = {}
+      if not obj[year][field][type]
+        obj[year][field][type] = {both: {}, men: {}, women: {}}
+      obj[year][field][type].both = {total: d.Geschlecht_Total, dual: d.Geschlecht_Dual , full: d.Geschlecht_Vollschulisch }
+      obj[year][field][type].women = {total: d.Frau_Total, dual: d.Frau_Dual , full: d.Frau_Vollschulisch }
+      obj[year][field][type].men = {total: d.Mann_Total, dual: d.Mann_Dual , full: d.Mann_Vollschulisch }
+  , -> deffered.resolve()
+  deffered.promise()
+
+loadSwitzerland = () ->
+  deffered = jQuery.Deferred()
+  d3.json 'ch.json', (err, geojson_switzerland) =>
     if err
       return console.error(err)
-    console.log 'Step 2 - load my data', data
-    # extract only cantons from the GeoJSON data
-    switzerland = topojson.feature(geojson_switzerland, geojson_switzerland.objects.cantons)
-    console.log switzerland
+
+    @switzerland = topojson.feature(geojson_switzerland, geojson_switzerland.objects.cantons)
+    deffered.resolve()
+  deffered.promise()
+
+loadRegions = () ->
+  deffered = jQuery.Deferred()
+  d3.json 'regions.json', (err, regions) ->
+    if err
+      return console.error(err)
+    region_names = []
+    @all_regions = regions.regions
+
+
+    for key, region of @all_regions
+      region_names.push(region.name)
+      @regions[region.name] = {features: [], color: region.color, origin: region.origin}
+    deffered.resolve()
+  deffered.promise()
+
+draw = ->
+  combineData()
+
+  @path = d3.geo.path().projection(null)
+
+  for name of @regions
+    drawRegion(name)
+
+  update()
+  resize()
+
+  d3.selectAll('g').style("display", "none")
+  @svg.select('#Westschweiz').style("display", "block")
+
+  addEventListner()
+
+addEventListner = ->
+  @svg.selectAll 'path.canton'
+  .on 'mousedown', tooltipShow
+  .on 'mouseover', tooltipShow
+  .on 'click', tooltipShow
+  .on 'mouseup', tooltipHide
+  .on 'mouseout', tooltipHide
+
+combineData = ->
+  for key, item of @cantonsObjects
+    for year, data of item
+      @switzerland.features.some (canton) =>
+        if canton.properties.abbr is key
+          canton.properties[year] = data
+
+          for region in @all_regions
+            for rcanton in region.cantons
+              if rcanton.name is key
+                canton.properties['region'] = region.name
+                @regions[region.name].features.push canton
 
 
 
-    d3.json 'regions.json', (err, regions) ->
-      if err
-        return console.error(err)
-      @region_names = []
-      @all_regions = regions.regions
 
-
-      for key, region of @all_regions
-        @region_names.push(region.name)
-        @regions[region.name] = {features: [], color: region.color, origin: region.origin}
-
-        # d3.select('#selector').selectAll('button').data(@region_names).enter().append('button').attr('class', 'button').text(String)
-
-      # split up cantons into the different regions
-
-      # Combine data sets
-      @data.forEach (row) =>
-        # for each row in data, look for the canton abbreviation in geodata
-        switzerland.features.some (canton) =>
-          if canton.properties.abbr == row.canton
-            # if found, add all data to the countries properties
-            for k of row
-              canton.properties[k] = row[k]
-            # add region prop
-            for region in @all_regions
-              for rcanton in region.cantons
-                if rcanton.name is row.canton
-                  canton.properties['region'] = region.name
-                  @regions[region.name].features.push canton
-
-
-      console.log @regions
-
-      console.log 'Step 3 - Check resulting dataset', switzerland
-      # store the SVG element
-      svg = d3.select('svg')
-      # Compute width and height of the SVG element (in pixels)
-      width = parseInt(d3.select('svg').style('width'))
-      height = parseInt(d3.select('svg').style('height'))
-      # define how paths are generated
-      path = d3.geo.path().projection(null)
-      # draw the cantons
-      console.log 'features'
-      console.log switzerland.features
-
-      # for info in data
-      #
-      console.log @regions
-
-# zentral: -370, -50
-# Ostschwiz: translate(-497,-2)
-# Westschweiz translate(-120,-72)
-# Mittelland translate(-265,-45)
-
-
-      westschweiz = svg.select('#westschweiz').selectAll('path.canton').data(@regions['Westschweiz'].features)
-      .enter()
-      .append('path')
-      .attr('class', 'canton')
-      .attr('d', path)
-      .style('fill', @regions['Westschweiz'].color)
-      .style('stroke', 'white')
-      .attr('stroke-width', 1)
-
-      ostschweiz = svg.select('#ostschweiz').selectAll('path.canton').data(@regions['Ostschweiz'].features)
-      .enter()
-      .append('path')
-      .attr('class', 'canton')
-      .attr('d', path)
-      .style('fill', @regions['Ostschweiz'].color)
-      .style('stroke', 'white')
-      .attr('stroke-width', 1)
-
-      zentralschweiz = svg.select('#zentralschweiz').selectAll('path.canton').data(@regions['Zentralschweiz'].features)
-      .enter()
-      .append('path')
-      .attr('class', 'canton')
-      .attr('d', path)
-      .style('fill', @regions['Zentralschweiz'].color)
-      .style('stroke', 'white')
-      .attr('stroke-width', 1)
-
-      mittelland = svg.select('#mittelland').selectAll('path.canton').data(@regions['Mittelland'].features)
-      .enter()
-      .append('path')
-      .attr('class', 'canton')
-      .attr('d', path)
-      .style('fill', @regions['Mittelland'].color)
-      .style('stroke', 'white')
-      .attr('stroke-width', 1)
-
-      d3.selectAll('g').style("display", "none")
-
-      resize()
 
       # for region in regions
 #   for canton in regions.cantons
@@ -186,7 +146,104 @@ d3.json 'ch.json', (err, geojson_switzerland) ->
     #     'translate(0,0)scale(' + 1.5 - scale(d.properties[key]) + ')rotate(0)'
     #   return
 
-    return
+    # return
+  # return
+
+drawRegion = (regionName) =>
+  features = @regions[regionName].features
+  @svg.select("##{regionName}").selectAll('path.canton').data(features)
+  .enter()
+  .append('path')
+  .attr('class', 'canton')
+  .attr('d', @path)
+  .style('fill', @regions[regionName].color)
+  .style('stroke', 'white')
+  .attr('stroke-width', 1)
+
+  @svg.select("##{regionName}").selectAll('text').data(features)
+  .enter()
+  .append 'g'
+  .attr 'class', 'text'
+  .attr 'transform', (d) ->
+    center = path.centroid(d)
+    center[0] = center[0] - 5
+    'translate('+center+')'
+  .append('text')
+  .text (d) ->
+    d.properties.abbr
+
+update = ->
+  for name, region of @regions
+    features = region.features
+    max = 0
+    min = 0
+    for feature in features
+      value = parseInt feature.properties[@year][@field][@type][@gender][@value]
+      if value >= max
+        max = value
+      if value <= min
+        min = value
+
+    color = d3.rgb region.color
+
+    scale = d3.scale.linear()
+            .domain([min, max])
+            .range([color.brighter(2), color.brighter(1), color, color.darker(1), color.darker(2)]);
+
+    @svg.select("##{name}").selectAll('path.canton').data(features)
+    .style 'fill', (d) =>
+      value = parseInt d.properties[@year][@field][@type][@gender][@value]
+      d3.rgb(scale(value))
+
+show = (elem) ->
+  region = elem.getAttribute('data-region')
+  d3.selectAll('g').style("display", "none")
+  @svg.select('#' + region).style("display", "block")
+
+resize = ->
+  # adjust things when the window size chan
+  width = parseInt(svg.style('width'))
+
+  console.log width
+
+  # scale each individual object according to theire own width/height and
+  # window width/height
+  # d3.selectAll("g").attr("transform", "scale(" + width / 600 + "), translate(" + width / 2 + ", 0)");
+  # probably all regions need ratio and translate
+
+  # translate(-centerX*(factor-1), -centerY*(factor-1))
+  # scale(factor)
+  # ratio = 1.25
+  # ratio = 1.25 if window.innerWidth <= 750
+
+  @svg.select('#Zentralschweiz').attr("transform", "scale(" + width/212 + "), translate(" + @regions['Zentralschweiz'].origin + ")");
+  @svg.select('#Mittelland').attr("transform", "scale(" + width/300 + "), translate(" + @regions['Mittelland'].origin + ")");
+  @svg.select('#Ostschweiz').attr("transform", "scale(" + width/340 + "), translate(" + @regions['Ostschweiz'].origin + ")");
+  @svg.select('#Westschweiz').attr("transform", "scale(" + width/400 + "), translate(" + @regions['Westschweiz'].origin + ")");
+
+
+  # map.style('width', width + 'px').style 'height', height + 'px'
+  # # resize the map
+  # map.select('.land').attr 'd', path
+  # map.selectAll('.state').attr 'd', path
   return
 
+tooltipShow = (d, i) =>
+  data = d.properties
+  value = data[@year][@field][@type][@gender][@value]
+  name = data.name
+  .replace('Ã¨', 'è')
+  .replace('Ã¢', 'â')
+  .replace('Ã¼', 'ü')
+  tip.transition()
+  .duration 200
+  .style 'opacity', .9
+  tip.html "<h5>#{name}</h5>
+           <p>#{value}</p>"
+  .style("left", (d3.event.pageX) + "px")
+  .style("top", (d3.event.pageY - 28) + "px");
 
+tooltipHide = ->
+  tip.transition()
+  .duration 500
+  .style 'opacity', 0
--- www/js/app.js
+++ www/js/app.js
@@ -1,132 +1,301 @@
-var resize, show, windowWidth;
+var addEventListner, combineData, draw, drawRegion, loadData, loadRegions, loadSwitzerland, resize, show, tip, tooltipHide, tooltipShow, update, windowWidth;
 
-show = function(elem) {
-  var region, svg;
-  region = elem.getAttribute('data-region');
-  svg = d3.select('svg');
-  d3.selectAll('g').style("display", "none");
-  return svg.select('#' + region).style("display", "block");
+this.years = [2010, 2011, 2012, 2013, 2014];
+
+this.year = 2014;
+
+this.field = 'Informatik';
+
+this.type = 'EFZ';
+
+this.gender = 'both';
+
+this.value = 'total';
+
+this.regions = [];
+
+this.cantonsObjects = {
+  'ZH': {},
+  'BE': {},
+  'LU': {},
+  'UR': {},
+  'SZ': {},
+  'OW': {},
+  'NW': {},
+  'GL': {},
+  'ZG': {},
+  'FR': {},
+  'SO': {},
+  'BS': {},
+  'BL': {},
+  'SH': {},
+  'AR': {},
+  'AI': {},
+  'SG': {},
+  'GR': {},
+  'AG': {},
+  'TG': {},
+  'TI': {},
+  'VD': {},
+  'VS': {},
+  'NE': {},
+  'GE': {},
+  'JU': {}
 };
 
-resize = function() {
-  var ratio, svg, test, width;
-  console.log('resize ');
-  svg = d3.select('svg');
-  width = parseInt(svg.style('width'));
-  ratio = 1;
-  if (window.innerWidth <= 750) {
-    ratio = 1.25;
-  }
-  test = width * -1;
-  svg.select('#zentralschweiz').attr("transform", "scale(" + ratio + "), translate(" + this.regions['Zentralschweiz'].origin + ")");
-  svg.select('#mittelland').attr("transform", "scale(" + ratio + "), translate(" + this.regions['Mittelland'].origin + ")");
-  svg.select('#ostschweiz').attr("transform", "scale(" + ratio + "), translate(" + this.regions['Ostschweiz'].origin + ")");
-  svg.select('#westschweiz').attr("transform", "scale(" + ratio + "), translate(" + this.regions['Westschweiz'].origin + ")");
-};
+this.svg = d3.select('svg');
+
+tip = d3.select("body").append("div").attr("class", "tip").style("opacity", 0);
 
 d3.select(window).on('resize', resize);
 
 windowWidth = window.innerWidth;
 
-if (windowWidth <= 750) {
-  console.log('Mobile');
-}
-
-this.regions = [];
-
-this.regions[0] = '1';
-
-d3.json('ch.json', function(err, geojson_switzerland) {
-  if (err) {
-    return console.error(err);
-  }
-  console.log('Step 1 - loading GeoJSON data', geojson_switzerland);
-  d3.csv('data.csv', (function(d) {
-    var k;
-    for (k in d) {
-      if (k !== 'canton') {
-        d[k] = +d[k];
-      }
+$(document).ready((function(_this) {
+  return function() {
+    var dataCounter, index, ref, year;
+    dataCounter = 0;
+    ref = _this.years;
+    for (index in ref) {
+      year = ref[index];
+      console.log(year);
     }
-    return d;
-  }), (function(_this) {
-    return function(err, data1) {
-      var switzerland;
-      _this.data = data1;
+    return loadData(2014).then(function() {
+      return loadSwitzerland().then(function() {
+        return loadRegions().then(function() {
+          return draw();
+        });
+      });
+    });
+  };
+})(this));
+
+loadData = function(year) {
+  var deffered;
+  deffered = jQuery.Deferred();
+  d3.csv(year + ".csv", function(d) {
+    var field, obj, type;
+    field = d.Ausbildungsfeld;
+    type = d.Ausbildungstyp;
+    obj = this.cantonsObjects[d.Canton];
+    if (obj) {
+      if (!obj[year]) {
+        obj[year] = {};
+      }
+      if (!obj[year][field]) {
+        obj[year][field] = {};
+      }
+      if (!obj[year][field][type]) {
+        obj[year][field][type] = {
+          both: {},
+          men: {},
+          women: {}
+        };
+      }
+      obj[year][field][type].both = {
+        total: d.Geschlecht_Total,
+        dual: d.Geschlecht_Dual,
+        full: d.Geschlecht_Vollschulisch
+      };
+      obj[year][field][type].women = {
+        total: d.Frau_Total,
+        dual: d.Frau_Dual,
+        full: d.Frau_Vollschulisch
+      };
+      return obj[year][field][type].men = {
+        total: d.Mann_Total,
+        dual: d.Mann_Dual,
+        full: d.Mann_Vollschulisch
+      };
+    }
+  }, function() {
+    return deffered.resolve();
+  });
+  return deffered.promise();
+};
+
+loadSwitzerland = function() {
+  var deffered;
+  deffered = jQuery.Deferred();
+  d3.json('ch.json', (function(_this) {
+    return function(err, geojson_switzerland) {
       if (err) {
         return console.error(err);
       }
-      console.log('Step 2 - load my data', data);
-      switzerland = topojson.feature(geojson_switzerland, geojson_switzerland.objects.cantons);
-      console.log(switzerland);
-      d3.json('regions.json', function(err, regions) {
-        var height, key, mittelland, ostschweiz, path, ref, region, svg, westschweiz, width, zentralschweiz;
-        if (err) {
-          return console.error(err);
-        }
-        this.region_names = [];
-        this.all_regions = regions.regions;
-        ref = this.all_regions;
-        for (key in ref) {
-          region = ref[key];
-          this.region_names.push(region.name);
-          this.regions[region.name] = {
-            features: [],
-            color: region.color,
-            origin: region.origin
-          };
-        }
-        this.data.forEach((function(_this) {
-          return function(row) {
-            return switzerland.features.some(function(canton) {
-              var i, k, len, rcanton, ref1, results;
-              if (canton.properties.abbr === row.canton) {
-                for (k in row) {
-                  canton.properties[k] = row[k];
-                }
-                ref1 = _this.all_regions;
-                results = [];
-                for (i = 0, len = ref1.length; i < len; i++) {
-                  region = ref1[i];
-                  results.push((function() {
-                    var j, len1, ref2, results1;
-                    ref2 = region.cantons;
-                    results1 = [];
-                    for (j = 0, len1 = ref2.length; j < len1; j++) {
-                      rcanton = ref2[j];
-                      if (rcanton.name === row.canton) {
-                        canton.properties['region'] = region.name;
-                        results1.push(this.regions[region.name].features.push(canton));
-                      } else {
-                        results1.push(void 0);
-                      }
-                    }
-                    return results1;
-                  }).call(_this));
-                }
-                return results;
-              }
-            });
-          };
-        })(this));
-        console.log(this.regions);
-        console.log('Step 3 - Check resulting dataset', switzerland);
-        svg = d3.select('svg');
-        width = parseInt(d3.select('svg').style('width'));
-        height = parseInt(d3.select('svg').style('height'));
-        path = d3.geo.path().projection(null);
-        console.log('features');
-        console.log(switzerland.features);
-        console.log(this.regions);
-        westschweiz = svg.select('#westschweiz').selectAll('path.canton').data(this.regions['Westschweiz'].features).enter().append('path').attr('class', 'canton').attr('d', path).style('fill', this.regions['Westschweiz'].color).style('stroke', 'white').attr('stroke-width', 1);
-        ostschweiz = svg.select('#ostschweiz').selectAll('path.canton').data(this.regions['Ostschweiz'].features).enter().append('path').attr('class', 'canton').attr('d', path).style('fill', this.regions['Ostschweiz'].color).style('stroke', 'white').attr('stroke-width', 1);
-        zentralschweiz = svg.select('#zentralschweiz').selectAll('path.canton').data(this.regions['Zentralschweiz'].features).enter().append('path').attr('class', 'canton').attr('d', path).style('fill', this.regions['Zentralschweiz'].color).style('stroke', 'white').attr('stroke-width', 1);
-        mittelland = svg.select('#mittelland').selectAll('path.canton').data(this.regions['Mittelland'].features).enter().append('path').attr('class', 'canton').attr('d', path).style('fill', this.regions['Mittelland'].color).style('stroke', 'white').attr('stroke-width', 1);
-        d3.selectAll('g').style("display", "none");
-        return resize();
-      });
+      _this.switzerland = topojson.feature(geojson_switzerland, geojson_switzerland.objects.cantons);
+      return deffered.resolve();
     };
   })(this));
-});
+  return deffered.promise();
+};
+
+loadRegions = function() {
+  var deffered;
+  deffered = jQuery.Deferred();
+  d3.json('regions.json', function(err, regions) {
+    var key, ref, region, region_names;
+    if (err) {
+      return console.error(err);
+    }
+    region_names = [];
+    this.all_regions = regions.regions;
+    ref = this.all_regions;
+    for (key in ref) {
+      region = ref[key];
+      region_names.push(region.name);
+      this.regions[region.name] = {
+        features: [],
+        color: region.color,
+        origin: region.origin
+      };
+    }
+    return deffered.resolve();
+  });
+  return deffered.promise();
+};
+
+draw = function() {
+  var name;
+  combineData();
+  this.path = d3.geo.path().projection(null);
+  for (name in this.regions) {
+    drawRegion(name);
+  }
+  update();
+  resize();
+  d3.selectAll('g').style("display", "none");
+  this.svg.select('#Westschweiz').style("display", "block");
+  return addEventListner();
+};
+
+addEventListner = function() {
+  return this.svg.selectAll('path.canton').on('mousedown', tooltipShow).on('mouseover', tooltipShow).on('click', tooltipShow).on('mouseup', tooltipHide).on('mouseout', tooltipHide);
+};
+
+combineData = function() {
+  var data, item, key, ref, results, year;
+  ref = this.cantonsObjects;
+  results = [];
+  for (key in ref) {
+    item = ref[key];
+    results.push((function() {
+      var results1;
+      results1 = [];
+      for (year in item) {
+        data = item[year];
+        results1.push(this.switzerland.features.some((function(_this) {
+          return function(canton) {
+            var j, len, rcanton, ref1, region, results2;
+            if (canton.properties.abbr === key) {
+              canton.properties[year] = data;
+              ref1 = _this.all_regions;
+              results2 = [];
+              for (j = 0, len = ref1.length; j < len; j++) {
+                region = ref1[j];
+                results2.push((function() {
+                  var k, len1, ref2, results3;
+                  ref2 = region.cantons;
+                  results3 = [];
+                  for (k = 0, len1 = ref2.length; k < len1; k++) {
+                    rcanton = ref2[k];
+                    if (rcanton.name === key) {
+                      canton.properties['region'] = region.name;
+                      results3.push(this.regions[region.name].features.push(canton));
+                    } else {
+                      results3.push(void 0);
+                    }
+                  }
+                  return results3;
+                }).call(_this));
+              }
+              return results2;
+            }
+          };
+        })(this)));
+      }
+      return results1;
+    }).call(this));
+  }
+  return results;
+};
+
+drawRegion = (function(_this) {
+  return function(regionName) {
+    var features;
+    features = _this.regions[regionName].features;
+    _this.svg.select("#" + regionName).selectAll('path.canton').data(features).enter().append('path').attr('class', 'canton').attr('d', _this.path).style('fill', _this.regions[regionName].color).style('stroke', 'white').attr('stroke-width', 1);
+    return _this.svg.select("#" + regionName).selectAll('text').data(features).enter().append('g').attr('class', 'text').attr('transform', function(d) {
+      var center;
+      center = path.centroid(d);
+      center[0] = center[0] - 5;
+      return 'translate(' + center + ')';
+    }).append('text').text(function(d) {
+      return d.properties.abbr;
+    });
+  };
+})(this);
+
+update = function() {
+  var color, feature, features, j, len, max, min, name, ref, region, results, scale, value;
+  ref = this.regions;
+  results = [];
+  for (name in ref) {
+    region = ref[name];
+    features = region.features;
+    max = 0;
+    min = 0;
+    for (j = 0, len = features.length; j < len; j++) {
+      feature = features[j];
+      value = parseInt(feature.properties[this.year][this.field][this.type][this.gender][this.value]);
+      if (value >= max) {
+        max = value;
+      }
+      if (value <= min) {
+        min = value;
+      }
+    }
+    color = d3.rgb(region.color);
+    scale = d3.scale.linear().domain([min, max]).range([color.brighter(2), color.brighter(1), color, color.darker(1), color.darker(2)]);
+    results.push(this.svg.select("#" + name).selectAll('path.canton').data(features).style('fill', (function(_this) {
+      return function(d) {
+        value = parseInt(d.properties[_this.year][_this.field][_this.type][_this.gender][_this.value]);
+        return d3.rgb(scale(value));
+      };
+    })(this)));
+  }
+  return results;
+};
+
+show = function(elem) {
+  var region;
+  region = elem.getAttribute('data-region');
+  d3.selectAll('g').style("display", "none");
+  return this.svg.select('#' + region).style("display", "block");
+};
+
+resize = function() {
+  var width;
+  width = parseInt(svg.style('width'));
+  console.log(width);
+  this.svg.select('#Zentralschweiz').attr("transform", "scale(" + width / 212 + "), translate(" + this.regions['Zentralschweiz'].origin + ")");
+  this.svg.select('#Mittelland').attr("transform", "scale(" + width / 300 + "), translate(" + this.regions['Mittelland'].origin + ")");
+  this.svg.select('#Ostschweiz').attr("transform", "scale(" + width / 340 + "), translate(" + this.regions['Ostschweiz'].origin + ")");
+  this.svg.select('#Westschweiz').attr("transform", "scale(" + width / 400 + "), translate(" + this.regions['Westschweiz'].origin + ")");
+};
+
+tooltipShow = (function(_this) {
+  return function(d, i) {
+    var data, name, value;
+    data = d.properties;
+    value = data[_this.year][_this.field][_this.type][_this.gender][_this.value];
+    name = data.name.replace('Ã¨', 'è').replace('Ã¢', 'â').replace('Ã¼', 'ü');
+    tip.transition().duration(200).style('opacity', .9);
+    return tip.html("<h5>" + name + "</h5> <p>" + value + "</p>").style("left", d3.event.pageX + "px").style("top", (d3.event.pageY - 28) + "px");
+  };
+})(this);
+
+tooltipHide = function() {
+  return tip.transition().duration(500).style('opacity', 0);
+};
 
 
